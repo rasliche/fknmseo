27b7724d1fc2c56f9a0cec897c49ca03ebb37825
EricR <rasliche@gmail.com>
2018-03-09 20:48:53 -0500

    Admin file hierarchy changes

---
+++ pages/admin.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+    <div>
+        <h1>This is the Admin page</h1>
+        <nuxt-link to="/admin/new">Add a new Organization</nuxt-link>
+        <nuxt-link to="/admin/organizations">All Organizations</nuxt-link>
+        <nuxt-link to="/admin/users">All Users</nuxt-link>
+        <nuxt-child />
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
