d8f4d66f1a7b36d2806a7bd7b50d79cc11cbf88e
EricR <rasliche@gmail.com>
2018-03-09 21:06:51 -0500

    Navigation Styling

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,9 +1,25 @@
 <template lang="html">
     <div>
         <h1>This is the Admin page</h1>
-        <nuxt-link to="/admin/new">Add a new Organization</nuxt-link>
-        <nuxt-link to="/admin/organizations">All Organizations</nuxt-link>
-        <nuxt-link to="/admin/users">All Users</nuxt-link>
+        <nuxt-link
+            class="bg-grey-light rounded mx-4 px-2"
+            active-class="bg-blue"
+            to="/admin/new-org">
+            Add a new Organization
+        </nuxt-link>
+        <nuxt-link
+            class="bg-grey-light rounded mx-4 px-2"
+            active-class="bg-blue"
+            to="/admin/organizations">
+            All Organizations
+        </nuxt-link>
+        <nuxt-link
+            class="bg-grey-light rounded mx-4 px-2"
+            active-class="bg-blue"
+            to="/admin/users">
+            All Users
+        </nuxt-link>
+
         <nuxt-child />
     </div>
 </template>
