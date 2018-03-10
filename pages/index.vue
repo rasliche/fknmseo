8a1124c2db3b6a4e1b0499d11c54f3950b562dcb
EricR <rasliche@gmail.com>
2018-03-09 21:18:21 -0500

    Front Page styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <template>
-  <section class="container">
-    <div>
-      <h1>fknmseo</h1>
-      <h2>FKNMS Education Portal</h2>
-    </div>
+  <section class="container main">
+    <h1 class="text-center">Florida Keys National Marine Sanctuary Education Portal</h1>
   </section>
 </template>
+
+<style>
+.main {
+    display: grid;
+    grid-gap: 20px;
+    grid-template-columns: repeat(6, 1fr);
+    border: solid black 3px;
+}
+
+h1 {
+    grid-column: span 6;
+}
+</style>
