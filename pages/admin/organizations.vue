f5f53265a6f1019b3db87b17cd22da4a68f15d80
EricR <rasliche@gmail.com>
2018-03-09 20:37:41 -0500

    Putting meat on the admin pages

---
+++ pages/admin/organizations.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+    <div>
+        <h1>admin/organizations</h1>
+        <ul>
+            <li v-for="org in 10">
+                <nuxt-link
+                    :to="{
+                        name: 'org-id',
+                        params: {id: org }
+                        }">
+                    Organization {{ org }}
+                </nuxt-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ pages/admin/users.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+    <div>
+        <h1>admin/users</h1>
+        <ul>
+            <li v-for="user in 25">User {{ user}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
